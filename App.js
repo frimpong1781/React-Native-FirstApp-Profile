b8b76787bb7b4c1015fe987522a404e607f6cc67
frimpong1781 <frimpong1781@yahoo.com>
2021-03-25 13:08:03 +0000

    Add App Component

--- App.js
+++ App.js
@@ -1,21 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Text, View, StyleSheet, Image} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+function App() {
+  return <View style={styles.container}>
+      <Image source={require('./assets/kwame.jpg')} style={styles.image} />
+
+      <View style={styles.detailContainer}>
+        <Text style={styles.label}>Name</Text>
+        <Text style={[styles.label, styles.info]}>James Frimpong</Text>
+      </View>
+
+      <View style={styles.detailContainer}>
+        <Text style={styles.label}>Email</Text>
+        <Text style={[styles.label, styles.info]}>frimpong1781@yahoo.com</Text>
+      </View>
+
+      <View style={styles.detailContainer}>
+        <Text style={styles.label}>Gender</Text>
+        <Text style={[styles.label, styles.info]}>Male</Text>
+      </View>
+  </View>
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container:{
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginHorizontal: 5,
+    backgroundColor: "teal"
   },
+
+  image:{
+    marginTop: 100,
+    height: 150,
+    width: 150,
+    marginBottom: 30,
+    alignSelf: "center",
+    borderRadius: 75
+  },
+
+  detailContainer:{
+    flexDirection: "row",
+    marginVertical: 3,
+    backgroundColor: "#fff"
+  },
+
+  label:{
+    borderWidth: 1,
+    borderColor: "black",
+    fontSize: 20,
+    flex: 2.5,
+    paddingHorizontal: 5
+  },
+
+  info:{
+    flex: 7.5
+  }
+
 });
+
+export default App;
